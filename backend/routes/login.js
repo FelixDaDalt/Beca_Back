9156aa2b80fdd622a6f4addd0e353310448e9cea
FelixDaDalt <felixdadalt@gmail.com>
2025-05-04 20:21:24 -0300

    .

--- backend/routes/login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const login_controller_1 = require("../controllers/login.controller");
-const router = (0, express_1.Router)();
-exports.router = router;
-router.post('/', login_controller_1.Login);
